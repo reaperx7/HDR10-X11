07bc174b16677f07f26486abae647be8abfcc670
James Powell <james4591@hotmail.com>
2025-06-26 18:44:11 -0700

    Update modesetting.c
    added append code for using a boolean detection system for xorg.conf style scripts.

--- hw/xfree86/drivers/modesetting/modesetting.c
+++ hw/xfree86/drivers/modesetting/modesetting.c
@@ -94,3 +94,73 @@ ApplyHDRMetadata(ScrnInfoPtr scrn, WindowPtr pWin)
     // Assume Vulkan swapchain is set up
     vkSetHdrMetadataEXT(ms->device, 1, &ms->swapchain, &metadata);
 }
+
+# This method add support for using a boolean toggle in xorg.conf scripts to enable automatic
+# detection and usage of HDR10 by the client if available and integration.
+# Add enableHDR10 to the ModesettingRec struct.
+# Call ParseHDR10Option during PreInit to read the configuration.
+# Check ms->enableHDR10 before enabling HDR in SetupVulkanHDR and ApplyHDRMetadata.
+
+
+#include <xf86.h>
+#include <xf86Opt.h>
+
+typedef struct _ModesettingRec {
+    // Existing fields...
+    VkPhysicalDevice physicalDevice;
+    VkDevice device;
+    VkBool32 hdrCapable;
+    Bool enableHDR10; // New field for HDR10 toggle
+} ModesettingRec;
+
+// Define option tokens
+static OptionInfoRec ModesettingOptions[] = {
+    { -1, "Enable_HDR10", OPTV_BOOLEAN, {0}, FALSE },
+    { -1, NULL, OPTV_NONE, {0}, FALSE }
+};
+
+static Bool
+ParseHDR10Option(ScrnInfoPtr scrn)
+{
+    ModesettingPtr ms = ModesettingGetPtr(scrn);
+    ms->enableHDR10 = FALSE; // Default: disabled
+
+    OptionInfoPtr options = xf86ParseOptionList(ModesettingOptions, scrn->options);
+    if (options) {
+        ms->enableHDR10 = xf86CheckBoolOption(options, "Enable_HDR10", FALSE);
+        xf86FreeOptionList(options);
+    }
+
+    LogMessage(X_DEBUG, "Modesetting: Enable_HDR10=%s\n", ms->enableHDR10 ? "on" : "off");
+    return ms->enableHDR10;
+}
+
+static Bool
+SetupVulkanHDR(ScrnInfoPtr scrn)
+{
+    ModesettingPtr ms = ModesettingGetPtr(scrn);
+    ms->hdrCapable = VK_FALSE;
+
+    if (!ParseHDR10Option(scrn)) {
+        LogMessage(X_INFO, "Modesetting: HDR10 disabled by configuration\n");
+        return FALSE;
+    }
+
+    // Existing Vulkan HDR setup code...
+    VkPhysicalDeviceProperties props;
+    vkGetPhysicalDeviceProperties(ms->physicalDevice, &props);
+    uint32_t extCount;
+    vkEnumerateDeviceExtensionProperties(ms->physicalDevice, NULL, &extCount, NULL);
+    VkExtensionProperties *exts = malloc(extCount * sizeof(VkExtensionProperties));
+    vkEnumerateDeviceExtensionProperties(ms->physicalDevice, NULL, &extCount, exts);
+    for (uint32_t i = 0; i < extCount; i++) {
+        if (strcmp(exts[i].extensionName, "VK_EXT_hdr_metadata") == 0) {
+            ms->hdrCapable = VK_TRUE;
+            break;
+        }
+    }
+    free(exts);
+
+    LogMessage(X_DEBUG, "Modesetting: HDR capable=%d\n", ms->hdrCapable);
+    return ms->hdrCapable;
+}
